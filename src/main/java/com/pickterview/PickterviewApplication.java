5c0351a81a97b8ec9e87d56bd528389bd9c9daa2
Mojin2 <aiduriaaa@gmail.com>
2025-06-04 12:48:12 +0900

    [INIT] 프로젝트 생성

---
+++ src/main/java/com/pickterview/PickterviewApplication.java
@@ -0,0 +1,13 @@
+package com.pickterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickterview/PickterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
